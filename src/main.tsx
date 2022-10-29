ad11ef6173b51611653fe9610cb2b74e13ef8d95
shimopino <shimoke4869@gmail.com>
2022-10-29 14:52:31 +0900

    mswの設定を追加

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const prepare = async () => {
+  if (import.meta.env.DEV) {
+    const { worker } = await import("./mocks/browser");
+    worker.start();
+  }
+};
+
+prepare().then(() =>
+  ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
 );
