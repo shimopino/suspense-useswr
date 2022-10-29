a4f4f7d86a26522f3b7096399a2c11c65c79415d
shimopino <shimoke4869@gmail.com>
2022-10-29 17:41:18 +0900

    検証用のために suspense: boolean 設定を追加

--- src/api/hooks/useUsers.ts
+++ src/api/hooks/useUsers.ts
@@ -8,15 +8,20 @@ type UsersResponse = {
   age: number;
 };
 
+type UseSWRConfig = {
+  suspense: boolean;
+};
+
 export const getUsers = async (path: string) => {
   const response = await client.get<UsersResponse[]>(path);
 
   return response.data;
 };
 
-export const useUsers = () => {
+export const useUsers = (config?: UseSWRConfig) => {
   const { data, mutate } = useSWR("/users", (path) => getUsers(path), {
     suspense: true,
+    ...config,
   });
 
   return { users: data, mutate };
