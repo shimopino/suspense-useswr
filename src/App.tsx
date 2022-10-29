9cdee45590bd67d250421214716505f3449eb6fb
shimopino <shimoke4869@gmail.com>
2022-10-29 14:28:52 +0900

    Viteでプロジェクトの初期化を行う

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      検証用
+    </div>
+  )
+}
+
+export default App
