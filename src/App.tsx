784a91ab2f975e2f2b5e3168be1199cff5494842
shimopino <shimoke4869@gmail.com>
2022-10-29 17:25:56 +0900

    同じキーを参照している場合、同じ箇所は同時に更新される

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import { useRealTime } from "./api/hooks/useRealTime";
-import { useUsers } from "./api/hooks/useUsers";
 import { RealTime } from "./components/RealTime";
 import { Users } from "./components/Users";
 
@@ -7,6 +5,7 @@ function App() {
   return (
     <>
       <h1>複数のデータをそのまま表示する場合</h1>
+      <RealTime />
       <RealTime />
       <Users />
     </>
